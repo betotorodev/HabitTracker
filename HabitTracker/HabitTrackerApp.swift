e31a4c36b37605b4482ba91df6f696f0a6fbd534
Beto Toro <beto.toro@zemoga.com>
2022-07-19 10:40:32 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Beto Toro on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
